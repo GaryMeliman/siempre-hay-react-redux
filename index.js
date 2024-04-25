13d3fe6d5f884322a1d72472a27fee8d8c35d7c4
Gary Meliman <gary.meliman@outlook.com>
2024-04-25 12:55:38 -0400

    index js y package

---
+++ index.js
@@ -0,0 +1,11 @@
+import episodesSlice from "./episodesSlice";
+import playerSlice from "./playerSlice";
+import store from "./store";
+import userSlice from "./userSlice";
+
+export {
+    episodesSlice,
+    playerSlice,
+    store,
+    userSlice
+}
